5bda920d318a22f1d2170d415375857311c4c514
Joseck Osugo <josugo26@gmail.com>
2024-07-25 23:58:29 +0300

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,7 +6,7 @@
     <table class="table">
         <thead>
             <tr>
-                <th>Nameeeeeeeeeeeeeeeeee</th>
+                <th>Nameeeeeeeeeeeeeeeee</th>
                 <th>Email</th>
                 <th>Created At</th>
             </tr>

a9dd654fab279b7bb4ad83c6cbd4f644ba5cd47d
Joseck <joseck@kali.gr>
2024-07-26 13:02:39 +0300

    Add necessary routes

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::delete('/users/{user}',[UserController::class,'destroy'])->name('users.destroy');
+Route::get('/users/{user}/edit',[UserController::class,'edit'])->name('users.edit');
+Route::put('/users/{user}',[UserController::class,'update'])->name('users.update');

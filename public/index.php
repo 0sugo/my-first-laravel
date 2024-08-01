2cb5d5869daa29dedbe304e9821bb517280372e9
Joseck Osugo <josugo26@gmail.com>
2024-08-01 23:59:41 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,7 +7,6 @@ define('LARAVEL_START', microtime(true));
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
-    q
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
